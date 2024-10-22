c40af79c4b856cbc92a241dffc5e2456c05ae1be
kota0314 <kota0314yama@gmail.com>
2024-10-22 22:10:54 +0900

    簡易的なオムニホイール制御をテスト機用に実装

--- include/attracts_interface/gamepad_node.hpp
+++ include/attracts_interface/gamepad_node.hpp
@@ -3,7 +3,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
-#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <std_msgs/msg/float32_multi_array.hpp>
 
 class Gamepad : public rclcpp::Node
 {
@@ -11,15 +12,17 @@ public:
     Gamepad();
 
 private:
-    void JoyCB(const sensor_msgs::msg::Joy::SharedPtr msg) const;
+    void JoyCB(const sensor_msgs::msg::Joy::SharedPtr msg);
 
 private:
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr cmd_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub_;
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
 
 private:
     double max_trans_vel_;
     double max_rot_vel_;
+    std::array<double, 6> positions_;
 };
 
 #endif
--- src/gamepad_node.cpp
+++ src/gamepad_node.cpp
@@ -7,20 +7,45 @@ Gamepad::Gamepad() : Node("gamepad_node")
     // RCLCPP_INFO(this->get_logger(), "max_trans_vel: %lf", max_trans_vel_);
     // max_rot_vel_ = this->get_parameter("max_rot_vel").as_double();
     // RCLCPP_INFO(this->get_logger(), "max_rot_vel: %lf", max_rot_vel_);
-    cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    cmd_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("cmd_vel", 10);
+    joint_state_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 10);
     joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&Gamepad::JoyCB, this, std::placeholders::_1));
 }
 
-void Gamepad::JoyCB(const sensor_msgs::msg::Joy::SharedPtr msg) const
+void Gamepad::JoyCB(const sensor_msgs::msg::Joy::SharedPtr msg)
 {
-    geometry_msgs::msg::Twist cmd_vel;
-    // cmd_vel.linear.x = max_trans_vel_ * msg->axes.at(1);
-    // cmd_vel.linear.y = -1.0 * max_trans_vel_ * msg->axes.at(0);
-    // cmd_vel.angular.z = -1.0 * max_rot_vel_ * msg->axes.at(3);
-    cmd_vel.linear.x = 0.2 * msg->axes.at(1);
-    cmd_vel.linear.y = -1.0 * 0.2 * msg->axes.at(0);
-    cmd_vel.angular.z = -1.0 * 0.4 * msg->axes.at(3);
+    double vec_x = 1.0 * msg->axes.at(1);
+    double vec_y = -1.0 * msg->axes.at(0);
+    double theta = std::atan2(vec_y, vec_x);
+    double norm = std::sqrt(std::pow(vec_x, 2) + std::pow(vec_y, 2));
+
+    double max_omni_vel = 0.2;
+    std_msgs::msg::Float32MultiArray cmd_vel;
+    cmd_vel.data.resize(6);
+    cmd_vel.data.at(0) = max_omni_vel * norm * std::sin(theta - M_PI * 0.25);
+    cmd_vel.data.at(1) = max_omni_vel * norm * std::sin(theta + M_PI * 0.25);
+    cmd_vel.data.at(2) = max_omni_vel * norm * std::sin(theta + M_PI * 1.25);
+    cmd_vel.data.at(3) = max_omni_vel * norm * std::sin(theta - M_PI * 1.25);
+    cmd_vel.data.at(4) = -1.0 * 0.4 * msg->axes.at(3);
+    cmd_vel.data.at(5) = 1.0 * 0.4 * msg->axes.at(4);
     cmd_pub_->publish(cmd_vel);
+
+    for (int i = 0; i < 6; i++)
+    {
+        positions_.at(i) += cmd_vel.data.at(i);
+    }
+
+    sensor_msgs::msg::JointState joint_state;
+    joint_state.header.stamp = get_clock()->now();
+    joint_state.name.emplace_back("right_front_wheel_joint");
+    joint_state.name.emplace_back("left_front_wheel_joint");
+    joint_state.name.emplace_back("right_back_wheel_joint");
+    joint_state.name.emplace_back("left_back_wheel_joint");
+    joint_state.position.emplace_back(positions_.at(0));
+    joint_state.position.emplace_back(positions_.at(1));
+    joint_state.position.emplace_back(positions_.at(2));
+    joint_state.position.emplace_back(positions_.at(3));
+    joint_state_pub_->publish(joint_state);
 }
 
 int main(int argc, char **argv)
